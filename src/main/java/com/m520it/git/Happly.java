94fd20cc95bbdb5df16a7d0b54586884e438d42b
igodven <igodven@m520it.com>
2019-06-13 22:06:15 +0800

    this fisdsa

--- src/main/java/com/m520it/git/Happly.java
+++ src/main/java/com/m520it/git/Happly.java
@@ -2,6 +2,7 @@ package com.m520it.git;
 
 public class Happly {
 public static void main(String[] args) {
-	System.out.println("我的第一次就没有了,我该怎么办!");
+	System.out.println("我的第一次就没有了,我该怎么办!$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+	System.out.println("nihaowuuj");
 }
 }
